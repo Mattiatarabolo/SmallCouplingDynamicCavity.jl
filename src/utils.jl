40a07cb3676e352637aa68024fa7fc699f4135aa
Mattiatarabolo <mattia.tarabolo@gmail.com>
2024-04-26 11:52:57 +0200

    Refactor ROC_curve function to use generic type for the second argument

--- src/utils.jl
+++ src/utils.jl
@@ -110,22 +110,22 @@ end
 
 
 """
-    ROC_curve(marg::Vector{Float64}, x::Vector{Float64})
+    ROC_curve(marg::Vector{Float64}, x::Vector{TI}) where {TI<:Integer}
 
 Compute the Receiver Operating Characteristic (ROC) curve and the Area Under the Curve (AUC) based on inferred marginal probabilities and true configurations.
 
 This function computes the ROC curve by sorting the marginal probabilities (`marg`) in decreasing order and sorting the true configuration (`x`) correspondingly. It then iterates over the sorted values to calculate the True Positive Rate (TPR) and False Positive Rate (FPR) at different thresholds, and computes the AUC.
 
 # Arguments
-- `marg::Vector{Float64}`: A vector containing the marginal probabilities of each node being infected at a fixed time.
-- `x::Vector{Float64}`: The true configuration of the nodes at the same fixed time.
+- `marg`: A vector containing the marginal probabilities of each node being infected at a fixed time.
+- `x`: The true configuration of the nodes at the same fixed time.
 
 # Returns
-- `fp_rates::Vector{Float64}`: Array of false positive rates corresponding to each threshold.
-- `tp_rates::Vector{Float64}`: Array of true positive rates corresponding to each threshold.
-- `auc::Float64`: The Area Under the Curve (AUC) of the ROC curve.
+- `fp_rates`: Array of false positive rates corresponding to each threshold.
+- `tp_rates`: Array of true positive rates corresponding to each threshold.
+- `auc`: The Area Under the Curve (AUC) of the ROC curve.
 """
-function ROC_curve(marg::Vector{Float64}, x::Vector{Float64})
+function ROC_curve(marg::Vector{Float64}, x::Vector{TI}) where {TI<:Integer}
     # Sort marg in decreasing order and reorder x correspondingly
     sorted_indices = sortperm(marg, rev=true)
     sorted_marg = marg[sorted_indices]
